f5113ffd2767e84d0101ac34518f983e84664fb9
Tracy Cai <tracycai@DN525j7n.SUNet>
2022-01-21 14:27:52 -0800

    initial commit to the IOS app

---
+++ Inflow/Inflow/InflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  InflowApp.swift
+//  Inflow
+//
+//  Created by Tracy Cai on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct InflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
