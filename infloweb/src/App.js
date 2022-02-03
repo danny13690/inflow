f16367182aa43322fc629ad836f74e5fb8a0b3b5
Ericw553 <46097277+Ericw553@users.noreply.github.com>
2022-02-03 04:27:07 -0800

    Update App.js
    Updated App.js with a functional table and logo positioning

--- infloweb/src/App.js
+++ infloweb/src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.png';
 import './App.css';
+import { useTable } from 'react-table'
 
 function App() {
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Jannik Obenhoff',
+        col2: 'St Louis, Missouri, USA',
+        col3: '767,5k',
+        col4: 'placeholder',
+        col5: '1',
+        col6: 'May 28, 2019',
+        col7: '1',
+        col8: 'placeholder',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Influencer',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Location',
+        accessor: 'col2',
+      },
+      {
+        Header: 'Total Reach',
+        accessor: 'col3',
+      },
+      {
+        Header: 'Networks',
+        accessor: 'col4',
+      },
+      {
+        Header: 'Proposals',
+        accessor: 'col5',
+      },
+      {
+        Header: 'Proposal Created',
+        accessor: 'col6',
+      },
+      {
+        Header: 'Content Since Last Proposal',
+        accessor: 'col7',
+      },
+      {
+        Header: 'Actions',
+        accessor: 'col8',
+      },
+    ],
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>Inflow</p>
       </header>
+      <body>
+        <table {...getTableProps()} style={{ border: 'solid 1px gray' }}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th
+                    {...column.getHeaderProps()}
+                    style={{
+                      background: 'white',
+                      color: 'black',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        style={{
+                          padding: '10px',
+                          border: 'white',
+                          background: 'white',
+                        }}
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </body>
     </div>
   );
 }
 
 export default App;
+
