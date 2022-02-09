632d9a58ab71116484068948efdc7df237894d66
Phillip Kim <phillipkkim@gmail.com>
2022-02-08 19:56:15 -0800

    cleanup

--- inflow-web/src/App.js
+++ inflow-web/src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
-import Home from "./components/home.component";
 import { MainPage } from "./components/pages/MainPage.js";
 
 function App() {
   return (<Router>
-    <div className="App">
       <Switch>
         <Route exact path='/' component={Login} />
         <Route path="/sign-in" component={Login} />
         <Route path="/sign-up" component={SignUp} />
         <Route path="/home" component={MainPage} />
       </Switch>
-      </div>
       </Router>
   );
 }
