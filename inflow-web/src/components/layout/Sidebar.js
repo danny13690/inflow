94a8f1ddfa13f620608cf60a71eb1ce1384da8e3
kjwu00 <kjwu00@stanford.edu>
2022-02-09 13:39:17 -0800

    Update Sidebar.js

--- inflow-web/src/components/layout/Sidebar.js
+++ inflow-web/src/components/layout/Sidebar.js
@@ -29,11 +29,11 @@ export class Sidebar extends React.Component {
     return (
         
         <Sider collapsible width={250} collapsed={collapsed} onCollapse={this.onCollapse}>
-          <Menu theme="dark" defaultSelectedKeys={[]} mode="inline">
+          {/* <Menu theme="dark" defaultSelectedKeys={[]} mode="inline">
             <Menu.Item key="1" icon={<img src={logo} className="App-logo" alt="logo"/>}>
               <img src={words} className="App-logo" alt="inflow"/>
             </Menu.Item>
-          </Menu>
+          </Menu> */}
  
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1" icon={<HomeOutlined />}>
