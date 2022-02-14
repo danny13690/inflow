59c5c657e798fafb43c16bca04caf6d66b9b02c6
kjwu00 <kjwu00@stanford.edu>
2022-02-14 13:52:11 -0800

    current code

--- inflow-web/src/components/CampaignList.js
+++ inflow-web/src/components/CampaignList.js
@@ -1,31 +1,43 @@
 import { List, Card } from 'antd';
 import { Row, Col } from 'antd';
 import { FirebaseError } from 'firebase/app';
-import { db } from "../index";
-import { collection, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 import '../App.css';
 
 export function CampaignList (props) {
 let listData = [];
 
 const getCampaigns = async function (values) {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBQ9pLA5iHoAijrd-Gf7XHOH_qhz7asE74",
+    authDomain: "inflow-3382f.firebaseapp.com",
+    projectId: "inflow-3382f",
+    storageBucket: "inflow-3382f.appspot.com",
+    messagingSenderId: "16173008953",
+    appId: "1:16173008953:web:dd099b7b74b4c64d1ee5e8",
+    measurementId: "G-8FCDTE5ZRY"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore();
   const campaignsCol = collection(db, "campaigns");
   const campaignsSnapshot = await getDocs(campaignsCol);
   const campaignsList = campaignsSnapshot.docs.map(doc => doc.data());
   console.log(campaignsList);
-  this.listData = [];
   let i = 1;
   campaignsList.forEach((doc) => {
-    if (i === 1) {
-      this.listData.push({
+      listData.push({
       id: i,
       title: doc.title,
       deliverables: doc.deliverables,
       compensation: doc.compensation,
     });
-  }
-    i += 1;
   })
+  console.log(listData);
+
 }
 
 getCampaigns();
