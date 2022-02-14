de4bcb8f317394463f0c68a7f8b7936453ef6f39
kjwu00 <kjwu00@stanford.edu>
2022-02-14 13:37:54 -0800

    connecting to firebase

--- inflow-web/src/components/CampaignList.js
+++ inflow-web/src/components/CampaignList.js
@@ -6,29 +6,31 @@ import { collection, getDocs } from "firebase/firestore";
 import '../App.css';
 
 export function CampaignList (props) {
-const listData = [];
+let listData = [];
 
 const getCampaigns = async function (values) {
   const campaignsCol = collection(db, "campaigns");
   const campaignsSnapshot = await getDocs(campaignsCol);
   const campaignsList = campaignsSnapshot.docs.map(doc => doc.data());
   console.log(campaignsList);
-  return campaignsList;
+  this.listData = [];
+  let i = 1;
+  campaignsList.forEach((doc) => {
+    if (i === 1) {
+      this.listData.push({
+      id: i,
+      title: doc.title,
+      deliverables: doc.deliverables,
+      compensation: doc.compensation,
+    });
+  }
+    i += 1;
+  })
 }
 
 getCampaigns();
 
-for (let i = 0; i < 5; i++) {
-  listData.push({
-    id: i,
-    title: `Kiehl's Brand Ambassador ${i}`,
-    deliverables:
-      <ul> <li> Instagram post per week minimum </li> <li> 500 likes per pots with #kiehls hashtag </li> <li> 1 unpacking video on story </li> </ul>,
-    compensation:
-      <ul> <li> 1x Kiehl's Ultra Facial Cream (150ml) </li> <li> 3x Kiehls Ultra Facial Cream (15ml) </li> <li> $100 per post </li> </ul>,
-  });
-}
-
+if (listData.length > 0 ){
 return (
   <List
     itemLayout="vertical"
@@ -60,4 +62,9 @@ return (
     )}
     />
     )
+} else {
+      return (
+        <p> Submit a Campaign! </p>
+      )
+}
 };
