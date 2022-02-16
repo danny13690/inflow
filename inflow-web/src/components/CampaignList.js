592b5f3a84a24eceb6df1fbaae097e2ddf4ba9c9
Phillip Kim <phillipkkim@gmail.com>
2022-02-16 01:32:16 -0800

    format campaign list, update Create campaign

--- inflow-web/src/components/CampaignList.js
+++ inflow-web/src/components/CampaignList.js
@@ -1,33 +1,19 @@
 import { List, Card } from 'antd';
 import { Row, Col } from 'antd';
-import { FirebaseError } from 'firebase/app';
-import { initializeApp } from "firebase/app";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import React from 'react';
 import '../App.css';
+import { db } from "../index";
+
 
 export class CampaignList extends React.Component {
   constructor(props) {
     super(props);
     this.state = { listData:[]};
+    this.getCampaigns();
   }
 
   getCampaigns = async (values) => {
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyBQ9pLA5iHoAijrd-Gf7XHOH_qhz7asE74",
-      authDomain: "inflow-3382f.firebaseapp.com",
-      projectId: "inflow-3382f",
-      storageBucket: "inflow-3382f.appspot.com",
-      messagingSenderId: "16173008953",
-      appId: "1:16173008953:web:dd099b7b74b4c64d1ee5e8",
-      measurementId: "G-8FCDTE5ZRY"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
-    const db = getFirestore();
     const campaignsCol = collection(db, "campaigns");
     const campaignsSnapshot = await getDocs(campaignsCol);
     const campaignsList = campaignsSnapshot.docs.map(doc => {let d = doc.data(); d['id'] = doc.id; return d;});
@@ -35,17 +21,12 @@ export class CampaignList extends React.Component {
     campaignsList.forEach((doc) => {
         newList.push({
         id: doc.id,
-        title: doc.title,
+        name: doc.name,
         deliverables: doc.deliverables,
         compensation: doc.compensation,
       });
     })
     this.setState({listData: newList});
-    console.log(this.state.listData);
-  }
-
-  componentDidMount() {
-    this.getCampaigns();
   }
 
   render() {
@@ -65,17 +46,31 @@ export class CampaignList extends React.Component {
 
           renderItem={item => (
             <List.Item
-              onClick={() => window.location.replace(`/home/InfluencerTable/${item.id}`)}
+              onClick={() => window.location.assign(`/home/InfluencerTable/${item.id}`)}
               key={item.id}
             >
-              <Card title={item.title} className="campaign-title">
+              <Card title={item.name} className="campaign-title">
                 <Row>
                 <Col span={12}>
-                {item.deliverables}
-                </Col>    
-                <Col span={12}>  
-                {item.compensation}
-                </Col>  
+                  <h>Deliverables</h>
+                <ul>
+                {item.deliverables.map(n => {
+                  return (
+                    <li>{n}</li>
+                  );
+                })}
+                </ul>
+                </Col> 
+                <Col span={12}>
+                <h>Compensation</h>
+                <ul>
+                {item.compensation.map(n => {
+                  return (
+                    <li>{n}</li>
+                  );
+                })}
+                </ul>
+                </Col>
                 </Row> 
               </Card>
             
--- inflow-web/src/components/NewCampaignForm.js
+++ inflow-web/src/components/NewCampaignForm.js
@@ -1,18 +1,31 @@
 import { Form, Input, Button, Checkbox } from 'antd';
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../index";
+import { collection, addDoc, refEqual } from "firebase/firestore";
+import { db, storage } from "../index";
 import { createPortal } from 'react-dom';
+import { ref, uploadBytes } from "firebase/storage";
 
 export const NewCampaignForm = () => {
+  let file = null;
+  function chooseFile(e) {
+    file = e.target.files[0];
+  }
+
   const addCampaign = async function (values) {
     console.log('Success:', values);
-    
-    await addDoc(collection(db, "campaigns"), {
-      title: values["title"],
-      deliverables: values["deliverables"],
-      compensation: values["compensation"]
+    const docRef = await addDoc(collection(db, "campaigns"), {
+      name: values["title"],
+      deliverables: [values["deliverables"]],
+      compensation: [values["compensation"]],
+      description: values["description"]
     });
-
+    if (file != null) {
+      const storageRef = ref(storage, 'campaigns/'+ docRef.id + '/image.jpg');
+      uploadBytes(storageRef, file).then(snapshot => {
+        console.log("successfully uploaded", snapshot);
+      }).catch(error => {
+        console.log(error.message);
+      });
+    }
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -46,6 +59,19 @@ export const NewCampaignForm = () => {
       </Form.Item>
 
       <Form.Item
+        label="Campaign Description"
+        name="description"
+        rules={[
+          {
+            required: true,
+            message: 'A description for your campaign',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
         label="Deliverables"
         name="deliverables"
         rules={[
@@ -71,6 +97,20 @@ export const NewCampaignForm = () => {
         <Input />
       </Form.Item>
 
+      <Form.Item
+        label="Picture"
+        name="picture"
+        rules={[
+          {
+            required: false,
+          },
+        ]}
+      >
+        <Input 
+          type='file'
+          onChange={chooseFile}
+        />
+      </Form.Item>
 
       <Form.Item
         wrapperCol={{
